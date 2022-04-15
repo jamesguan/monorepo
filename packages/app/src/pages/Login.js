63aa845b77cc77b4cce4deff0b28f0f49a139679
James Guan <jamesguan94@gmail.com>
2022-04-15 10:24:42 -0400

    [JAMES] - Neotag items

--- packages/app/src/pages/Login.js
+++ packages/app/src/pages/Login.js
@@ -1,78 +1,25 @@
 // https://codesandbox.io/s/4xq2qpzw79?file=/src/FormStateToRedux.js:0-355
 // https://codesandbox.io/s/l3p44o94kl
 // https://styled-components.com/docs/basics#styling-any-components
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import PropTypes from "prop-types";
 import Row from 'react-bootstrap/Row';
 import styled from 'styled-components';
 
+import { connect } from 'react-redux';
 import { Form as FinalForm, Field } from 'react-final-form';
+
+import { passwordSchema, phoneSchema, validate } from '@monorepo/common/lib/utils/validators';
+import { normalizeOnlyNumbers, normalizePhone } from '@monorepo/common/lib/utils/normalize';
 
 import InputAdapter from '@monorepo/common/lib/components/fields/adapters/InputAdapter';
 
 import NeoTagLogoImage from '@monorepo/common/lib/images/svg/neotag/neotaglogo_mark.svg';
 import NeoTagNameImage from '@monorepo/common/lib/images/svg/neotag/neotaglogo_text.svg';
-
-const FormComponent = ({
-  formID,
-  handleSubmit,
-  hasSubmitErrors,
-  hasValidationErrors,
-  optionsFoods,
-  meta,
-  reset,
-  pristine,
-  submitError,
-  submitFailed,
-  submitting,
-  values,
-}) => {
-  return (
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId={`${formID}-phone-control`}>
-          <Form.Label className="text-light">Phone</Form.Label>
-          <Field
-            required
-            aria-describedby={`${formID}-phone-help`}
-            className="bg-secondary text-light"
-            component={InputAdapter}
-            name="phone"
-            placeholder="Enter your phone number" 
-            type="phone"
-          />
-          <Form.Text className="text-muted" id={`${formID}-phone-help`}>
-            Datarates may apply. US customers only
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId={`${formID}-password-control`}>
-          <Form.Label className="text-light">Password</Form.Label>
-          <Field
-            required
-            aria-describedby={`${formID}-password-help`}
-            className="bg-secondary text-light"
-            component={InputAdapter}
-            name="password"
-            placeholder="Enter your password" 
-            type="password"
-          />
-          <Button className="text-muted" variant="link">Forgot password</Button>
-        </Form.Group>
-        <div className="d-grid gap-2">
-          <Button size="lg" type="submit">
-            Login
-          </Button>
-        </ div>
-      </Form>
-  );
-};
-
-const handleSubmit = (values) => {
-  console.log(values);
-};
 
 const PageContainer = styled.div`
   align-items: center;
@@ -91,7 +38,14 @@ const NeoTagLogo = styled.img`
   margin: 8px 12px;
 `;
 
-const Login = (props) => {
+const formId = 'login';
+
+const Login = ({
+  login,
+}) => {
+  const handleSubmit = (values) => {
+    login({ username: values.phone, password: values.password });
+  };
   return (
     <PageContainer className="bg-secondary">
       <Container>
@@ -103,23 +57,60 @@ const Login = (props) => {
           <Col className="justify-content-center" md={6} lg={4}>
             <LoginContainer className="bg-dark">
               <Card.Body>
-                <FinalForm
-                  /**
-                   * @see https://github.com/final-form/react-final-form#-performance-optimization-through-subscriptions-
-                   */
-                  subscription={{
-                    hasSubmitErrors: true,
-                    hasValidationErrors: true,
-                    pristine: true,
-                    submitError: true,
-                    submitFailed: true,
-                    submitting: true
-                  }}
-                  onSubmit={handleSubmit}
-                  component={FormComponent}
-                  formID='login'
-                  { ...props }
-                />
+              <FinalForm
+        /**
+        * @see https://github.com/final-form/react-final-form#-performance-optimization-through-subscriptions-
+        */
+        subscription={{
+          hasSubmitErrors: true,
+          hasValidationErrors: true,
+          pristine: true,
+          submitError: true,
+          submitFailed: true,
+          submitting: true
+        }}
+        onSubmit={handleSubmit}
+        render={({ handleSubmit, values, form, pristine }) => (
+          <Form onSubmit={handleSubmit}>
+            <Field
+              required
+              formId={formId}
+              id="phone"
+              label="Phone"
+              infoText="Data rates may apply"
+              aria-describedby={`${formId}-phone-help`}
+              className="bg-secondary text-light"
+              component={InputAdapter}
+              name="phone"
+              parse={normalizeOnlyNumbers}
+              format={normalizePhone}
+              placeholder="Enter your phone number" 
+              type="phone"
+              valid={meta => meta.visited && meta.valid}
+              validate={validate(phoneSchema)}
+            />
+            <Field
+                required
+                aria-describedby={`${formId}-password-help`}
+                formId={formId}
+                id="password"
+                label="Password"
+                className="bg-secondary text-light"
+                component={InputAdapter}
+                name="password"
+                placeholder="Enter your password" 
+                type="password"
+                validate={validate(passwordSchema)}
+            />
+            <Button className="text-muted" variant="link">Forgot password</Button>
+            <div className="d-grid gap-2">
+              <Button  disabled={pristine} size="lg" type="submit">
+                Login
+              </Button>
+            </ div>
+          </Form>
+        )}
+      />
                 <span className="text-light">Don't have an account?</ span><Button className="text-light" variant="link">Sign up</Button>
               </ Card.Body>
             </ LoginContainer>
@@ -130,4 +121,13 @@ const Login = (props) => {
   );
 };
 
-export default Login;
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = ({ user: { handleLogin } }) => ({
+  login: ({ username, password }) => handleLogin({ username, password}),
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- packages/app/src/pages/Welcome.js
+++ packages/app/src/pages/Welcome.js
@@ -1,3 +1,4 @@
+// http://css3.bradshawenterprises.com/cfimg/
 import { useState } from 'react';
 
 import Col from 'react-bootstrap/Col';
@@ -7,13 +8,132 @@ import Toast from 'react-bootstrap/Toast';
 
 import styled from 'styled-components';
 
+import FadeInSection from '@monorepo/common/lib/components/containers/FadeInSection';
+import Station from '@monorepo/common/lib/images/svg/station.svg';
 import Unit from '@monorepo/common/lib/images/svg/unit_w_shadow.svg';
+
+import Discord from '@monorepo/common/lib/images/svg/discord_circular.svg';
+import UnitBlue from '@monorepo/common/lib/images/svg/neotag_unit_blue-01.svg';
+import UnitCyan from '@monorepo/common/lib/images/svg/neotag_unit_cyan-01.svg';
+import UnitPurple from '@monorepo/common/lib/images/svg/neotag_unit_purple-01.svg';
+import UnitRed from '@monorepo/common/lib/images/svg/neotag_unit_red-01.svg';
+import UnitYellow from '@monorepo/common/lib/images/svg/neotag_unit_yellow-01.svg';
 
 
 import Loader from '../components/Loader';
 import Nav from '../components/Nav';
 import Layout from '../components/Layout';
+import { Fade } from 'react-bootstrap';
 
+const CustomFadeInSection = styled(FadeInSection)`
+  &.white-background {
+    background-color: rgba(255, 100, 255, 0.6);
+  }
+  
+  margin-left: calc((-100vw + 100%) / 2);
+  margin-right: calc((-100vw + 100%) / 2);
+  padding: 3%;
+`;
+
+const BackgroundContainer = styled(Container)``;
+
+const FadeImage = styled.img`
+  position:absolute;
+  left:0;
+  max-width: 1200px;
+  object-fit: contain;
+`;
+
+const ImageRoller = styled.div`
+  display: block;
+  position: relative;
+  transform: translateY(40%);
+  min-height: 300px;
+  width: 500px;
+  @keyframes FadeInAndOutImages {
+    0% {
+      opacity:1;
+    }
+    17% {
+      opacity:1;
+    }
+    25% {
+      opacity:0;
+    }
+    92% {
+      opacity:0;
+    }
+    100% {
+      opacity:1;
+    }
+  }
+
+  img:nth-of-type(1) {
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: 8s;
+    animation-direction: alternate;
+    animation-name: FadeInAndOutImages;
+    animation-delay: 6s;
+  }
+  img:nth-of-type(2) {
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: 8s;
+    animation-direction: alternate;
+    animation-name: FadeInAndOutImages;
+    animation-delay: 4s;
+  }
+  img:nth-of-type(3) {
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: 8s;
+    animation-direction: alternate;
+    animation-name: FadeInAndOutImages;
+    animation-delay: 2s;
+  }
+  img:nth-of-type(4) {
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    animation-duration: 8s;
+    animation-direction: alternate;
+    animation-name: FadeInAndOutImages;
+    animation-delay: 0;
+  }
+`;
+
+const BackgroundImage = styled.div`
+  bottom: 0;
+  height: calc(100vh - 120px);
+  left: 0;
+  position: fixed;
+  pointer-events: none;
+  right: 0;
+  top: 60px;
+  width: 100vw;
+  &::before {
+    background-attachment: fixed;
+    background-image: url(${Unit});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    content: "";
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    opacity: 0.35;
+  }
+`;
+
+const DiscordLogo = styled.img`
+  max-width: 200px;
+`;
+
+const UnitImage = styled.img`
+  max-width: 1200px;
+`;
 
 const data = [
   {
@@ -28,26 +148,54 @@ const data = [
   }
 ];
 
-const UnitImage = styled.img`
-  max-width: 1200px;
-`;
+const pageContent = [
+  {
+    name: 'Unit',
+    description: 'NeoTag brings you the most advanced lasertag system ever created. Scroll down to learn more.',
+    image: Unit,
+  },
+  {
+    name: 'Arena in a box',
+    description: 'No facility, no problem! Our system allows you to deploy a laser tag arena everywhere and anywhere.',
+    image: Station,
+  },
+  {
+    name: 'Pick your color',
+    description: 'Play with your friends or teams represented by your favorite colors',
+    component: (
+      <ImageRoller>
+        <FadeImage class="bottom" src={UnitBlue} />
+        <FadeImage class="top" src={UnitRed} />
+        <FadeImage class="bottom" src={UnitYellow} />
+        <FadeImage class="top" src={UnitPurple} />
+      </ImageRoller>
+    )
+  },
+  {
+    name: 'Join our Discord',
+    description: (
+      <>
+        Curious for more? Check out our <a href="https://discord.gg/CaFZ44m5Bp" target="_blank">Discord</a> for more
+      </>
+    ),
+    component: (
+      <a href="https://discord.gg/CaFZ44m5Bp" target="_blank"><DiscordLogo src={Discord} /></a>
+    ),
+  },
+];
 
 const Welcome = () => {
   const [notifications, setNotifications] = useState(data);
 
-  const clearNotification = (index) => () => {
-    console.log('Index', index);
-    console.log("notifications", notifications);
-    console.log("data", data);
-    data.splice(index);
-    setNotifications(data);
-    console.log(notifications);
-    console.log('data', data);
+  const clearNotification = (removeAtIndex) => () => {
+    setNotifications(notifications.filter((_, index) => index !== removeAtIndex ));
   };
 
   return (
+    <>
+    <BackgroundImage src={Unit} />
     <Layout>
-      <Container fluid className="bg-secondary">
+      <BackgroundContainer fluid className="bg-secondary">
         <Row className="d-flex justify-content-center">
           {notifications.map(({title, time, message}, index) => (
             <Col key={time} xs={12}>
@@ -62,22 +210,31 @@ const Welcome = () => {
             </ Col>
           ))}
         </Row>
-        <Row className="align-items-center">
-          <Col className="d-flex justify-content-center" md={6}>
-            <h4 className="text-light">NeoTag brings you the most advanced lasertag system ever created. Scroll down to learn more.</ h4>
-          </Col>
-          <Col className="d-flex justify-content-center" md={6}>
-            <UnitImage src={Unit} />
-          </Col>
-        </Row>
+        {
+          pageContent.map(({ component, name, image, description}, index) => (
+            <div key={name}>
+            <CustomFadeInSection className={`${index % 2 !== 0 && 'white-background'}`}>
+              <Row className={`align-items-center ${index % 2 !== 0 && 'flex-row-reverse'}`}>
+                <Col className="d-flex justify-content-center" md={6}>
+                  <h4 className="text-light">{description}</ h4>
+                </Col>
+                <Col className="d-flex justify-content-center" md={6}>
+                  {image && <UnitImage src={image} />}
+                  {component && component}
+                </Col>
+              </Row>
+            </CustomFadeInSection>
+            </div>
+          ))
+        }
         <Row>
           <Col>
             <Loader />
           </Col>
         </Row>
-      </Container>
+      </BackgroundContainer>
     </ Layout>
-
+    </>
   )
 };
 export default Welcome;
