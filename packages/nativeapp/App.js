ccabb66aa2a9f11106ecfcee80d2728a60c87531
James Guan <jamesguan94@gmail.com>
2022-04-18 00:35:47 -0400

    [JAMES] - Added React Native Navigation

--- packages/nativeapp/App.js
+++ packages/nativeapp/App.js
@@ -7,11 +7,9 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
@@ -26,7 +24,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -52,7 +50,7 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -61,7 +59,6 @@ const App: () => Node = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
--- packages/nativeapp/README.md
+++ packages/nativeapp/README.md
@@ -9,3 +9,7 @@ const nativeapp = require('@monorepo/nativeapp');
 
 // TODO: DEMONSTRATE API
 ```
+
+
+Opinionated installation with wix React Native Navigation
+https://wix.github.io/react-native-navigation/docs/installing
